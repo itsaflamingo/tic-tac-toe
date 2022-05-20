7eb57e33e56e698cfcca3fa281b13be785f08b0d
Elizabeth <elizabeth.pinero.r@gmail.com>
2022-05-20 14:59:15 -0400

    Fixed issue with file system.

--- script.js
+++ script.js
@@ -1,233 +1,303 @@
-const submit = document.querySelector('.submit');
-const modal = document.querySelector('.modal');
-const compOption = document.querySelector('.compOption');
-const gameLevels = document.querySelectorAll('.levels');
-const gameboard = document.querySelector('.gameBoard');
-const restart = document.querySelector('.restart');
-let easy = document.querySelector('#easy');
-let med = document.querySelector('#med');
-let impossible = document.querySelector('#imp');
-const gameDiv = document.querySelectorAll('.gameDiv');
+const submit = document.querySelector(".submit");
+const modal = document.querySelector(".modal");
+const compOption = document.querySelector(".compOption");
+const gameLevels = document.querySelectorAll(".levels");
+const gameboard = document.querySelector(".gameBoard");
+const restart = document.querySelectorAll(".restart");
+let easy = document.querySelector("#easy");
+let med = document.querySelector("#med");
+let impossible = document.querySelector("#imp");
+let displayWin = document.querySelector('.display-winner');
+let displayName = document.querySelector('.display-name');
 
 
-let selectedDifficulty;
+let turnCounter = 0;
+let playerOneArray = [];
+let playerTwoArray = [];
+let match = [];
+let arr = [];
 
-easy.addEventListener('click', (e) => (selectedDifficulty = "easy"));
-med.addEventListener('click', (e) => (selectedDifficulty = "med"));
-impossible.addEventListener('click', (e) => (selectedDifficulty = "imp"));
+compOption.addEventListener("click", () => toggleComp());
+restart.forEach((button) => button.addEventListener('click', (e) => gameController().restartGame()));
 
-compOption.addEventListener('click', () => toggleComp());
-restart.addEventListener('click', () => gameController().sendRestart());
 
 toggleComp = () => {
-    compOption.classList.toggle('active');
-}
+  compOption.classList.toggle("active");
+};
 
-(formRetrieve = () => {
+const formRetrieve = () => {
+  //create factory that returns object with everything we need.
 
-//create factory that returns object with everything we need. 
+  modal.classList.add("invis");
 
-    modal.classList.add('invis');
+  const playerOneInput = document.querySelector("#playerOneInput").value;
+  let playerTwoInput = document.querySelector("#playerTwoInput").value;
 
-    const playerOneInput = document.querySelector('#playerOneInput').value;
-    let playerTwoInput;
+  if (compOption.classList.contains('active')) {
+    playerTwoInput = "Computer";
+  }
 
-    if (document.querySelector('#playerTwoInput').value !== '') {
-        playerTwoInput = document.querySelector('#playerTwoInput').value;
-    }
-    else {
-        playerTwoInput = 'Computer';
-    }
+  let sendName = gameController();
+  sendName.playerNames(playerOneInput, playerTwoInput);
 
-    return {
-        playerOneInput,
-        playerTwoInput,
-    };
-
-});
+  return {
+    playerOneInput,
+    playerTwoInput,
+  };
+};
 
 submit.addEventListener("click", (e) => {
-    
-    let form = formRetrieve();
-
-    const sendNames = playerFactory.playerInfo(form);
-    
+  let form = formRetrieve();
+  return;
 });
 
-const playerFactory = (() => {
+const gameController = () => {
 
-    const playerInfo = (form) => {
+  let send;
 
-        console.log(form);
+  const playerNames = (playerOne, playerTwo) => {
+    let sendNametoDisplay = displayController.displayNames(playerOne, playerTwo);
+  }
 
-        //get each player's score, send to GC
+  const winnerObj = () => [
+    [1, 2, 3],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
 
-        const gameControl = gameController.displayName(form);
+  const playerChoice = (toggle, chosenNum) => {
 
+    if (toggle === true) {
+      playerOneArray.push(chosenNum);
+      currentPlayer = 'O';
+      if (turnCounter >= 4) {
+        send = scoreCalculator();
+        send.currentPlayerPattern(currentPlayer, playerOneArray);
+        scoreCalculator();
+      }
+      turnCounter++;
+    }
+    if (toggle === false) {
+      playerTwoArray.push(chosenNum);
+      currentPlayer = 'X';
+      if (turnCounter >= 4) {
+        send = scoreCalculator();
+        send.currentPlayerPattern(currentPlayer, playerTwoArray);
+        scoreCalculator();
+      }
+      turnCounter++;
     }
 
-    let playerScore = (patterns) => {
+  }
 
-        //patterns representing possible win combinations
-        let pattern = [[1, 2, 3], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [4, 5, 6], [7, 8, 9]];
+  //possible win combinations
+  const winningPattern = winnerObj();
 
-        //if x's turn, get array from playerOnePattern
-        if(obj.toggle === true) {
-            match = obj.playerOnePattern;
-            arr = Array.from(String(obj.playerOnePattern), Number);
+  //loop interates over each pattern. Match will return array with same numbers as pattern if they match. If array has 3 numbers, send info of winner to game.
+  const scoreCalculator = () => {
+
+    const currentPlayerPattern = (player, array) => {
+      //if x's turn (true)
+      if (player === 'X') {
+        return arr = array.join().split(',');
+      }
+
+      //if o's turn (false)
+      else if (player === 'O') {
+        return arr = array.join().split(',');
+      }
+    };
+    for (i = 0; i < winningPattern.length; i++) {
+
+      match = winningPattern[i].filter(function (item) {
+        if (displayWin.classList.contains('invis')) {
+          let index = arr.indexOf(`${item}`);
+          let num = `${item}` === arr[index];
+
+          return num;
         }
-
-        //if o's turn, get array from playerTwoPattern
-        else if (obj.toggle === false) {
-            match = obj.playerTwoPattern;
-            arr = Array.from(String(obj.playerTwoPattern), Number);
-        }
-
-        //loop interates over each pattern. Match will return array with same numbers as pattern if they match. 
-        for(i = 0; i < pattern.length; i++) {
-
-            match = pattern[i].filter(function(item) {
-                let index = arr.indexOf(item);
-                let num = item === arr[index];
-                return num;
-            });
-
-            if(match.length === 3) {
-                let display = gameController(obj);
-                display.displayWin();
-            }
-
-        }
+      });
+      if (match.length === 3) {
+        arr = '';
+        displayController.displayWinner(currentPlayer);
+      }
 
     }
 
     return {
-        playerInfo
+      currentPlayerPattern
+    };
+  }
+
+
+  const computer = (availableTiles, toggle) => {
+
+    let index = Math.floor(Math.random() * availableTiles.length);
+    let num = availableTiles[index];
+
+    if (toggle === false && availableTiles.length >= 1) {
+      document.getElementById(num).click();
     }
+
+  }
+
+  const restartGame = () => {
+
+    displayWin.classList.add('invis');
+
+    gameDiv.forEach(div => {
+      let element = document.getElementsByClassName("para");
+      if (element.length > 0) {
+        element[0].remove();
+      }
+
+    })
+
+    let names = document.getElementsByClassName('name-para');
+    names[0].remove();
+
+    let p = document.getElementsByClassName('winner');
+    p[0].remove();
+
+
+
+    //reset global variables
+    displayController.resetTiles();
+    turnCounter = 0;
+    playerOneArray = [];
+    playerTwoArray = [];
+    match = [];
+    arr = [];
+
+    toggle = false;
+    compOption.classList.remove('active');
+    modal.classList.remove('invis');
+  }
+
+  return {
+    playerChoice,
+    scoreCalculator,
+    computer,
+    restartGame,
+    playerNames
+  };
+};
+
+const createGameBoard = (() => {
+
+  //create board matrix. Takes array property in array-like object, uses map() to replace all undefined with an index number, and raises that index by 1 until it reaches the stated length. 
+  let divArray = Array.from({ length: 9 }, (undefined, index) => ++index);
+  id = 0;
+
+  divArray.forEach((div) => {
+
+    ++id;
+
+    const gamediv = document.createElement('div');
+    gamediv.classList.add('gameDiv');
+    gamediv.setAttribute("id", id);
+
+    gameboard.appendChild(gamediv);
+
+  });
+
+  return {
+    divArray
+  }
 
 })();
 
-const gameController = (() => {
+const gameDiv = document.querySelectorAll('.gameDiv');
+gameDiv.forEach((div) => div.addEventListener('click', (e) => displayController.playerChoice(e, div)));
 
-    const displayName = (form) => {
-
-        const dispController = displayController.displayName(form);
-
-    }
-
-    return {
-
-        displayName: displayName
-
-    }
-
-})();
 
 const displayController = (() => {
 
-    const nameDisplay = document.querySelector('.display-name');
+  let currentPlayer = 'X';
+  let index;
+  let toggle = true;
+  let availableTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    const playerChoice = () => {
 
-        const playerOneChoice = () => {
+  const playerChoice = (e, div) => {
 
-            const p = document.createElement('p');
-            p.setAttribute('class', 'para');
-            playerOne = document.createTextNode('X');
+    const playerX = () => {
+      currentPlayer = 'X';
+      toggle = false;
+    };
+    const playerO = () => {
+      currentPlayer = 'O';
+      toggle = true;
+    };
 
-            div.appendChild(p);
-            p.appendChild(playerOne);
+    (function togglePlayers() {
+      toggle ? playerX() : playerO();
+    })();
 
-            //next selection will trigger playerTwoComp
-            toggle = true;
+    index = availableTiles.indexOf(parseInt(e.target.id));
+    let numChoice = availableTiles.splice(index, 1);
+    parseInt(numChoice[0]);
 
-        }
 
-        const playerTwoChoice = () => {
+    if (div.hasChildNodes() === false) {
+      const p = document.createElement('p');
+      p.setAttribute('class', 'para');
+      div.appendChild(p);
 
-            const p = document.createElement('p');
-            p.setAttribute('class', 'para');
-            playerTwo = document.createTextNode('O');
-
-            div.appendChild(p);
-            p.appendChild(playerTwo);
-
-            toggle = false;
-
-        }
-
-        (function togglePlayers () {
-            toggle ? playerOneChoice() : playerTwoChoice();
-        })();
-    }
-    
-
-    const displayName = (form) => {
-
-        let namesDisplay; 
-
-        const p = document.createElement('p');
-        p.setAttribute('class', 'name-para');
-
-        if(compOption.classList.contains('active')) {
-
-            namesDisplay = document.createTextNode(`Hi, ${form.playerOneInput}!`);
-
-        }
-
-        else {
-
-            if(modal.classList.contains('invis')) {
-                namesDisplay = document.createTextNode(`Hi, ${form.playerOneInput} & ${form.playerTwoInput}!`);
-            }
-            else {
-                return;
-            }
-
-        }
-
-        nameDisplay.appendChild(p);
-        p.appendChild(namesDisplay);
-
+      player = document.createTextNode(`${currentPlayer}`);
+      p.appendChild(player);
     }
 
-    return {
-        displayName,
+    currentPlayerToScore(toggle, numChoice, index);
+
+    if (compOption.classList.contains('active')) {
+      let comp = gameController();
+      comp.computer(availableTiles, toggle);
     }
+
+  }
+
+  const currentPlayerToScore = (toggle, chosenNum) => gameController().playerChoice(toggle, chosenNum);
+
+  const displayNames = (playerOne, playerTwo) => {
+
+    const p = document.createElement('p');
+    p.setAttribute('class', 'name-para');
+    displayName.appendChild(p);
+    player = document.createTextNode(`Hi, ${playerOne} and ${playerTwo}!`);
+    p.appendChild(player);
+
+  }
+
+  const displayWinner = () => {
+
+    displayWin.classList.remove('invis');
+    let p = document.createElement('p');
+    p.setAttribute('class', 'winner')
+    win = document.createTextNode(`${currentPlayer} wins!`)
+    p.appendChild(win);
+    displayWin.appendChild(p);
+
+  }
+
+  const resetTiles = () => {
+    return availableTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  }
+
+  return {
+    playerChoice,
+    toggle,
+    displayWinner,
+    displayNames,
+    resetTiles
+  }
 
 })();
 
-const gameBoard = (() => {
-    
-    //create board matrix. Takes array property in array-like object, uses map() to replace all undefined with an index number, and raises that index by 1 until it reaches the stated length. 
-    let divArray = Array.from({length: 9}, (undefined, index) => ++index);
-    id = 0;
 
-    gameDiv.forEach((div) => div.addEventListener('click', (e) => {
-
-        console.log(e);
-        // displayController.playerChoice(e, div)
-    
-    }));
-
-    divArray.forEach((div, divArray) => {
-
-        ++id;
-
-        const gamediv = document.createElement('div');
-        gamediv.classList.add('gameDiv');
-        gamediv.setAttribute("id", id);
-        
-        gameboard.appendChild(gamediv);
-    });
-
-    
-
-    return {
-
-        divArray
-
-    }
-
-})();
 
